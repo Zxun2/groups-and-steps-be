9cac46dd69ccd2d8f934ddf814234c307ff49810
Zong Xun <zxun2000@gmail.com>
2021-12-13 15:27:57 +0800

    added jwt

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,14 +3,16 @@ class TodosController < ApplicationController
   
     # GET /todos
     def index
-      @todos = Todo.all
-      json_response(@todos)
+        # get current user todos
+        @todos = current_user.todos
+        json_response(@todos)
     end
   
     # POST /todos
     def create
-      @todo = Todo.create!(todo_params)
-      json_response(@todo, :created)
+        # create todos belonging to current user
+        @todo = current_user.todos.create!(todo_params)
+        json_response(@todo, :created)
     end
   
     # GET /todos/:id
@@ -34,7 +36,7 @@ class TodosController < ApplicationController
   
     def todo_params
       # whitelist params
-      params.permit(:title, :created_by)
+      params.permit(:title)
     end
   
     def set_todo
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items
   end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+
 end
