d73105f25b31188a23f4f0e5dfc5aea675bd4dc7
Zong Xun <zxun2000@gmail.com>
2021-12-13 20:17:20 +0800

    added email validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,8 @@
 class Todo < ApplicationRecord
     # model association
     has_many :items, dependent: :destroy
-  
+    belongs_to :user
+    
     # validations
     validates_presence_of :created_by, :title
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     # Validations
     validates_presence_of :name, :email, :password_digest
     validates :email, presence: true, uniqueness: { case_sensitive: false }
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :name, length: { minimum: 2 }
     # validates :bio, length: { maximum: 500 }, presence: false
     validates :password, length: { in: 6..20 }
